82951469c28d23129d9638f8b98b9ee87bd0aa4e
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-06-29 16:07:13 +0300

    fucking lol

--- CUDA.cuh
+++ CUDA.cuh
@@ -2,44 +2,60 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void WeightCreation(float* weight, int size) {                                                          //инициализация весов
+//инициализация весов
+__global__ void WeightCreation(float* weight, int size) {                         
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	if (index < size)
-		weight[index] = /*1 / (1 + exp2f(-index))*/0.384;
+		weight[index] = 1 / (1 + exp2f(-index / 4000));
 }
 
+//обнуление delw
+__global__ void DelwNull(float* delw, int sum) {
+	int index = blockIdx.x + blockIdx.y * gridDim.x;
+	if (index < sum)
+		delw[index] = 0;
+}
+
+//присваивание входов
 __global__ void InputData(float* data, float* out, int size) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	if (index < size)
 		out[index] = data[index];
 }
 
+//сумматор
 __global__ void Sumfunc(int coat, int Wnum, int Onum, float* weight, float* out, int dop) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	float net = 0;
 
-	if (index < dop) {
-		for (int i = 0; i < coat; i++) net = net + weight[Wnum + index * coat + i] * out[Onum + i];                //выходы
+	//if (index < dop) {
+		for (int i = 0; i < coat; i++) {
+			net = net + weight[Wnum + index * coat + i] * out[Onum + i];
+		}
 		out[Onum + coat + index] = 1 / (1 + exp2f(-net));
-	}
+	//}
 }
 
+//первая дельта
 __global__ void Delta(float* outO, float* out, float* del, int Onum, int size) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 
-	if (index < size)
+	//if (index < size)
 		del[index] = (outO[index] - out[Onum + index]) * (1 - out[Onum + index]) * out[Onum + index];
 }
 
+//последующие дельты
 __global__ void DeltaN(int Dnum, int Wnum, int Onum, float* del, float* weight, float* out, int coat, int n) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	float per = 0;
 
-	for (int i = 0; i < coat; i++)
+	for (int i = 0; i < coat; i++) {
 		per = per + del[Dnum + i] * weight[Wnum + index + n * i];
+	}
 	del[Dnum + coat + index] = (1 - out[Onum + index]) * out[Onum + index] * per;
 }
 
+//изменение весов
 __global__ void Deltaw(float* weight, float* del, float* out, float* delw, int Dnum, int Onum, int Wnum, int coat, int n) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	float grad = 0;
@@ -52,3 +68,7 @@ __global__ void Deltaw(float* weight, float* del, float* out, float* delw, int D
 }
 
 /*-------------------работает-------------------*/
+
+__global__ void InputImage() {
+
+}
