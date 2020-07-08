bf21bec1a9b2f701bffdf6186264682da6a23cf1
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-08 14:26:38 +0300

    Update Graph.cu

--- Graph.cu
+++ Graph.cu
@@ -6,41 +6,42 @@
 
 #include "func.cuh"
 
-const int layer = 4;
+const int layer = 3;
 
 int main() {
-	int WeightSum = 0, NeuralSum = 0, n[layer] = { 11, 64, 256, 784 };
+	int WeightSum = 0, NeuralSum = 0, n[layer] = { 32, 256, 784 };
 	float* del, * delw, * weight, * out, * Inp, * Oout;
 	clock_t t1;
 	std::string filename;
 
 	for (int i = 0; i < layer; i++)
 		NeuralSum = n[i] + NeuralSum;
-	std::cout << "Neurals: " << NeuralSum << std::endl;
+	std::cout << "Neurals: " << NeuralSum * 3 << std::endl;
 
 	for (int i = 0; i < layer - 1; i++)
 		WeightSum = n[i] * n[i + 1] + WeightSum;
-	std::cout << "Weights: " << WeightSum << std::endl;
+	std::cout << "Weights: " << WeightSum * 3 << std::endl;
 	std::cout << std::endl;
 
-	float* outO = new float[n[layer - 1]];
+	//float* InputDataArr = new float[n[0]];
+	float* outO = new float[n[layer - 1] * 3];
 
 	for (int i = 0; i < n[layer - 1]; i++)
 		outO[i] = 0;
 
-	cudaMalloc((void**)&out, NeuralSum * sizeof(float));
+	cudaMalloc((void**)&out, NeuralSum * 3 * sizeof(float));
 	cudaMalloc((void**)&del, (NeuralSum - n[0]) * sizeof(float));
-	cudaMalloc((void**)&weight, WeightSum * sizeof(float));
-	cudaMalloc((void**)&delw, WeightSum * sizeof(float));
-	cudaMalloc((void**)&Inp, n[0] * sizeof(float));
-	cudaMalloc((void**)&Oout, n[layer - 1] * sizeof(float));
+	cudaMalloc((void**)&weight, WeightSum * 3 * sizeof(float));
+	cudaMalloc((void**)&delw, WeightSum * 3 * sizeof(float));
+	cudaMalloc((void**)&Inp, n[0] * 3 * sizeof(float));
+	cudaMalloc((void**)&Oout, n[layer - 1] * 3 * sizeof(float));
 
 	DelwNull << < WeightSum, 1 >> > (delw, WeightSum);
 
 	cv::Mat image = cv::imread("E:\\Foton\\ngnl_data\\training\\0\\1 (1).png");
 	/*cv::imshow("Out", image);
 	cv::waitKey(10000);*/
-	cv::Mat result(image.rows, image.cols, CV_8UC1);
+	cv::Mat result(image.rows, image.cols, CV_8UC3);
 	Input(n[layer - 1], outO, Oout, image);
 	DataCheck(WeightSum, weight, delw);
 
@@ -48,11 +49,10 @@ int main() {
 	for (int adm = 0; adm < 1000; adm++) {
 		std::cout << "Iter #" << adm + 1 << std::endl;
 		for (int l = 0; l < 1; l++) {
-			for (int num = 0; num < 1; num++) {
-				for (int k = 0; k < 5000; k++) {
-					cv::Mat image = cv::imread("E:\\Foton\\ngnl_data\\training\\" + std::to_string(0) + "\\1 (" + std::to_string(k + 1) + ").png");
-					Input(n[layer - 1], outO, Oout, image);
-					InputGen(n[0], Inp, out);
+			for (int num = 0; num < 5000; num++) {
+				for (int k = 0; k < 10; k++) {
+					cv::Mat image = cv::imread("E:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(num + 1) + ").png");
+					InputGen(n[0], NeuralSum, Inp, out);
 					Iteration(n, layer, NeuralSum, WeightSum, weight, out, delw, Oout, outO, del);
 					Out(NeuralSum, layer, n, out, result);
 				}
@@ -72,6 +72,8 @@ int main() {
 		std::cout << "Backup" << std::endl;
 		delete[] Bweight;
 		delete[] Bdelw;
+		fweight.close();
+		fdelw.close();
 
 		std::ofstream config("E:\\Foton\\ngnl_data\\backup\\config.txt");
 		config << 1;
