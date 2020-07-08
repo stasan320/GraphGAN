299f95359321021d12d25f143d4aef3767dceaf8
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-08 14:29:09 +0300

    Update CUDA.cuh

--- CUDA.cuh
+++ CUDA.cuh
@@ -9,11 +9,11 @@ __global__ void DelwNull(float* delw, int sum) {
 }
 
 //присваивание входов
-__global__ void InputData(float* data, float* out, int size) {
+__global__ void InputData(float* data, float* out, int size, int p, int NeuralSum) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 
 	if (index < size)
-		out[index] = data[index];
+		out[NeuralSum * p + index] = data[size * p + index];
 	//out[index] = 0.542 + (exp2f(2 * index) - 1) / (exp2f(2 * index) + 1);
 }
 
@@ -30,10 +30,10 @@ __global__ void Sumfunc(int layer, int Wnum, int Onum, float* weight, float* out
 }
 
 //первая дельта
-__global__ void Delta(float* outO, float* out, float* del, int Onum, int size) {
+__global__ void Delta(float* outO, float* out, float* del, int Onum, int size, int p, int n) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	//if (index < size)
-	del[index] = (outO[index] - out[Onum + index]) * (1 - out[Onum + index]) * out[Onum + index];                                     //sigm
+	del[index] = (outO[p * n + index] - out[Onum + index]) * (1 - out[Onum + index]) * out[Onum + index];                                     //sigm
 	//del[index] = (outO[index] - out[Onum + index]) * (1 - out[Onum + index]) * (1 + out[Onum + index]);									//tang
 }
 
@@ -69,7 +69,6 @@ __global__ void Clayer(float* weight, float* out, int Onum) {
 		net = net + weight[index * 16 + i] * out[index * 16 + i];
 	out[Onum + index] = (exp2f(2 * net) - 1) / (exp2f(2 * net) + 1);
 }
-
 __global__ void ConvDeltaW(float* weight, float* out, float* del, float* delw, int Dnum, int n) {
 	int index = blockIdx.x + blockIdx.y * gridDim.x;
 	float grad = 0;
@@ -83,62 +82,67 @@ __global__ void ConvDeltaW(float* weight, float* out, float* del, float* delw, i
 void Iteration(int* n, int layer, int NeuralSum, int WeightSum, float* weight, float* out, float* delw, float* Oout, float* outO, float* del) {
 	int Wnum = 0, Onum = 0, Dnum = 0;
 
-	for (int i = 0; i < (layer - 1); i++) {
-		Sumfunc << <n[i + 1], 1 >> > (n[i], Wnum, Onum, weight, out, n[i + 1]);										//int layer, int Wnum, int Onum, float* weight, float* out
-		Wnum = Wnum + n[i] * n[i + 1];
-		Onum = Onum + n[i];
-	}
+	for (int p = 0; p < 3; p++) {
+		Wnum = WeightSum * p;
+		Onum = NeuralSum * p;
+		
+		/*Dnum = (NeuralSum - n[0]) * p;*/
+		for (int i = 0; i < (layer - 1); i++) {
+			Sumfunc << <n[i + 1], 1 >> > (n[i], Wnum, Onum, weight, out, n[i + 1]);										//int layer, int Wnum, int Onum, float* weight, float* out
+			Wnum = Wnum + n[i] * n[i + 1];
+			Onum = Onum + n[i];
+		}
 
-	Onum = NeuralSum - n[layer - 1];
-	Delta << <n[layer - 1], 1 >> > (Oout, out, del, Onum, n[layer - 1]);
-	Wnum = WeightSum;
+		Delta << <n[layer - 1], 1 >> > (Oout, out, del, Onum, n[layer - 1], p, n[layer - 1]);
 
-	for (int j = 0; j < layer - 1; j++) {
-		Onum = Onum - n[layer - 2 - j];
-		Wnum = Wnum - n[layer - 2 - j] * n[layer - 1 - j];
-		DeltaN << <n[layer - 2 - j], 1 >> > (Dnum, Wnum, Onum, del, weight, out, n[layer - 1 - j], n[layer - 2 - j]);					    //int Dnum, int Wnum, int Onum, float* del, float* weight, float* out
-		Dnum = Dnum + n[layer - 1 - j];
-	}
+		for (int j = 0; j < layer - 1; j++) {
+			Onum = Onum - n[layer - 2 - j];
+			Wnum = Wnum - n[layer - 2 - j] * n[layer - 1 - j];
+			DeltaN << <n[layer - 2 - j], 1 >> > (Dnum, Wnum, Onum, del, weight, out, n[layer - 1 - j], n[layer - 2 - j]);					    //int Dnum, int Wnum, int Onum, float* del, float* weight, float* out
+			Dnum = Dnum + n[layer - 1 - j];
+		}
 
-	Wnum = WeightSum;
-	Dnum = 0;
-	Onum = NeuralSum - n[layer - 1];
+		Wnum = WeightSum * (p + 1);
+		Dnum = 0;
+		Onum = NeuralSum * (p + 1) - n[layer - 1];
 
-	for (int j = 0; j < layer - 1; j++) {
-		Onum = Onum - n[layer - 2 - j];
-		Wnum = Wnum - n[layer - 1 - j] * n[layer - 2 - j];
-		Deltaw << < n[layer - 2 - j], 1 >> > (weight, del, out, delw, Dnum, Onum, Wnum, n[layer - 1 - j], n[layer - 2 - j]);				//float* weight, float* del, float* out, float* delw, int Dnum, int Onum, int Wnum, int layer, int n
-		Dnum = Dnum + n[layer - 1 - j];
+		for (int j = 0; j < layer - 1; j++) {
+			Onum = Onum - n[layer - 2 - j];
+			Wnum = Wnum - n[layer - 1 - j] * n[layer - 2 - j];
+			Deltaw << < n[layer - 2 - j], 1 >> > (weight, del, out, delw, Dnum, Onum, Wnum, n[layer - 1 - j], n[layer - 2 - j]);				//float* weight, float* del, float* out, float* delw, int Dnum, int Onum, int Wnum, int layer, int n
+			Dnum = Dnum + n[layer - 1 - j];
+		}
 	}
 }
 
 void Input(int n, float* outO, float* Oout, cv::Mat image) {
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			float per = 0;
-			per = image.at<cv::Vec3b>(i, j)[0];
-			per = per / 255;
-			outO[i * image.cols + j] = per;
-			//std::cout << outO[i * image.cols + j] << std::endl;
+	for (int p = 0; p < 3; p++) {
+		for (int i = 0; i < image.rows; i++) {
+			for (int j = 0; j < image.cols; j++) {
+				float per = 0;
+				per = image.at<cv::Vec3b>(i, j)[p];
+				per = per / 255;
+				outO[p * image.cols * image.rows + i * image.cols + j] = per;
+			}
 		}
 	}
-	cudaMemcpy(Oout, outO, n * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(Oout, outO, n * 3 * sizeof(float), cudaMemcpyHostToDevice);
 }
 
 void Out(int NeuralSum, int layer, int* n, float* out, cv::Mat result) {
-	float* weights = new float[NeuralSum];
-	int Onum = NeuralSum - n[layer - 1];
-	cudaMemcpy(weights, out, NeuralSum * sizeof(float), cudaMemcpyDeviceToHost);
+	float* weights = new float[NeuralSum * 3];
+	cudaMemcpy(weights, out, NeuralSum * 3 * sizeof(float), cudaMemcpyDeviceToHost);
 
-
-	for (int i = 0; i < result.rows; i++) {
-		for (int j = 0; j < result.cols; j++) {
-			float per = 0;
-			per = weights[Onum + i * result.cols + j];
-			per = per * 255;
-			per = ceil(per);
-			//std::cout << per << std::endl;
-			result.at<uchar>(i, j) = per;
+	for (int p = 0; p < 3; p++) {
+		int Onum = NeuralSum * (p + 1) - n[layer - 1];
+		for (int i = 0; i < result.rows; i++) {
+			for (int j = 0; j < result.cols; j++) {
+				float per = 0;
+				per = weights[Onum + i * result.cols + j];
+				per = ceil(per * 255);
+				//std::cout << per << std::endl;
+				result.at<cv::Vec3b>(i, j)[p] = per;
+			}
 		}
 	}
 	cv::imshow("Out", result);
@@ -146,29 +150,32 @@ void Out(int NeuralSum, int layer, int* n, float* out, cv::Mat result) {
 	delete[] weights;
 }
 
+//инициализация весов
 void WeightGen(float* weight, float* delw, int WeightSum) {
-	float* data = new float[WeightSum];
+	float* data = new float[WeightSum * 3];
 	float max = 3, min = -3;
 	srand(static_cast<unsigned int>(time(0)));
 
 	for (int i = 0; i < WeightSum; i++) {
 		data[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
 	}
-	cudaMemcpy(weight, data, WeightSum * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(delw, data, WeightSum * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(weight, data, WeightSum * 3 * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(delw, data, WeightSum * 3 * sizeof(float), cudaMemcpyHostToDevice);
 	delete[] data;
 }
 
-void InputGen(int n, float* Inp, float* out) {
-	float* data = new float[n];
+//инициализация входов
+void InputGen(int n, int NeuralSum, float* Inp, float* out) {
+	float* data = new float[n * 3];
 	float max = 1, min = -1;
 	srand(static_cast<unsigned int>(time(0)));
 
 	for (int i = 0; i < n; i++) {
 		data[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
 	}
-	cudaMemcpy(Inp, data, n * sizeof(float), cudaMemcpyHostToDevice);
-	InputData << <n, 1 >> > (Inp, out, n);
+	cudaMemcpy(Inp, data, n * 3 * sizeof(float), cudaMemcpyHostToDevice);
+	for(int i = 0; i < 3; i++)
+		InputData << <n, 1 >> > (Inp, out, n, i, NeuralSum);
 	delete[] data;
 }
 
