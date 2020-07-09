7441b420abd3def68cdee2ad6480679a946d5b73
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-09 23:58:41 +0300

    Update Graph.cu

--- Graph.cu
+++ Graph.cu
@@ -9,8 +9,10 @@
 const int layer = 3;
 
 int main() {
-	int WeightSum = 0, NeuralSum = 0, n[layer] = { 16, 64, 784 };
-	float* del, * delw, * weight, * out, * Inp, * Oout;
+	int WeightSum = 0, NeuralSum = 0, n[layer] = { 16, 64, 784 }, int dop = 0,
+		DisWeightSum = 0, DisNeuralSum = 0, nc[layer] = {784, 64, 1 };
+	float * del, * delw, * weight, * out, * Inp, * Oout,
+		  * Disdel, * Disdelw, * Disweight, * Disout, * DisInp, * DisOout;
 	clock_t t1;
 	std::string filename;
 
@@ -29,9 +31,12 @@ int main() {
 	/*---------перевести в const---------*/
 
 	float* outO = new float[n[layer - 1] * 3];
+	float* DisoutO = new float[nc[layer - 1] * 3];
 
 	for (int i = 0; i < n[layer - 1] * 3; i++)
 		outO[i] = 0;
+	for (int i = 0; i < nc[layer - 1] * 3; i++)
+		DisoutO[i] = 0;
 
 	cudaMalloc((void**)&out, NeuralSum * 3 * sizeof(float));
 	cudaMalloc((void**)&del, (NeuralSum - n[0]) * sizeof(float));
@@ -40,12 +45,20 @@ int main() {
 	cudaMalloc((void**)&Inp, n[0] * 3 * sizeof(float));
 	cudaMalloc((void**)&Oout, n[layer - 1] * 3 * sizeof(float));
 
+	cudaMalloc((void**)&Disout, NeuralSum * 3 * sizeof(float));
+	cudaMalloc((void**)&Disdel, (NeuralSum - n[0]) * sizeof(float));
+	cudaMalloc((void**)&Disweight, WeightSum * 3 * sizeof(float));
+	cudaMalloc((void**)&Disdelw, WeightSum * 3 * sizeof(float));
+	cudaMalloc((void**)&DisInp, n[0] * 3 * sizeof(float));
+	cudaMalloc((void**)&DisOout, n[layer - 1] * 3 * sizeof(float));
+
 	DelwNull << < WeightSum, 1 >> > (delw, WeightSum);
 
 	cv::Mat image = cv::imread("E:\\Foton\\ngnl_data\\training\\0\\1 (9).png");
 	cv::Mat result(image.rows, image.cols, CV_8UC3);
 	InputOutImage(n[layer - 1], outO, Oout, image);
 	DataCheck(WeightSum, weight, delw, 0);
+	//DataCheck(DisWeightSum, Disweight, Disdelw, 1);
 
 	t1 = clock();
 	for (int adm = 0; adm < 1000; adm++) {
@@ -53,12 +66,17 @@ int main() {
 		for (int l = 0; l < 1; l++) {
 			for (int num = 0; num < 5000; num++) {
 				for (int k = 0; k < 1; k++) {
+					OutDiffer(dop, nc[0], DisNeuralSum, DisInp, Disout, DisoutO, image, 0);
+				}
+
+				for (int k = 0; k < 1; k++) {
 					/*cv::Mat image = cv::imread("E:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(num + 1) + ").png");
 					Input(n[layer - 1], outO, Oout, image);*/
 					Random(n[0], NeuralSum, Inp, out, 0);
 					GlobalSumFunc(n, layer, NeuralSum, WeightSum, weight, out);
 					Iteration(n, layer, NeuralSum, WeightSum, weight, out, delw, Oout, outO, del);
 					OutOutImage(NeuralSum, layer, n, out, result);
+					//std::cout << WeightSum << std::endl;
 				}
 			}
 		}
