6b626c83a888df6438d7c2f06593cc5aa2050479
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-18 15:24:32 +0300

    Update gan.h

--- experimental/CPU/gan.h
+++ experimental/CPU/gan.h
@@ -1,69 +1,52 @@
-void Sum(double* weight, double* out, int* n) {
-	for (int i = 0; i < n[1]; i++) {
-		double net = 0;
-		for (int j = 0; j < n[0]; j++) {
-			net = weight[i * n[0] + j] * out[j] + net;
+void SumG(float* weight, float* out, int* n, int layer) {
+	int Onum = 0, Wnum = 0;
+	for (int k = 0; k < (layer - 1); k++) {
+		for (int i = 0; i < n[k + 1]; i++) {
+			float net = 0;
+			for (int j = 0; j < n[k]; j++) {
+				net = weight[Wnum + i * n[k] + j] * out[Onum + j] + net;
+			}
+			out[Onum + n[k] + i] = 1 / (1 + exp(-net));
 		}
-		//out[n[0] + i] = 1 / (1 + exp(-net));
-		/*if (net < 0) {
-			out[n[0] + i] = 0;
-		}
-		else {
-			out[n[0] + i] = net;
-		}*/
-		out[n[0] + i] = 1 / (1 + exp(-net));
-	}
-
-	for (int i = 0; i < n[2]; i++) {
-		double net = 0;
-		for (int j = 0; j < n[1]; j++) {
-			net = weight[n[0] * n[1] + i * n[1] + j] * out[n[0] + j] + net;
-		}
-		//out[n[0] + n[1] + i] = (exp(2 * net) - 1) / (1 + exp(2 * net));
-		out[n[0] + n[1] + i] = 1 / (1 + exp(-net));
-		/*if (net < 0) {
-			out[n[0] + n[1] + i] = 0;
-		}
-		else {
-			out[n[0] + n[1] + i] = net;
-		}*/
+		Wnum = Wnum + n[k] * n[k + 1];
+		Onum = Onum + n[k];
 	}
 }
 
-void SumD(double* weight, double* out, int* n) {
-	for (int i = 0; i < n[1]; i++) {
-		double net = 0;
-		for (int j = 0; j < n[0]; j++) {
-			net = weight[i * n[0] + j] * out[j] + net;
+void SumD(float* weight, float* out, int* n, int layer) {
+	int Onum = 0, Wnum = 0;
+	for (int k = 0; k < (layer - 1); k++) {
+		for (int i = 0; i < n[k + 1]; i++) {
+			float net = 0;
+			for (int j = 0; j < n[k]; j++) {
+				net = weight[Wnum + i * n[k] + j] * out[Onum + j] + net;
+			}
+			out[Onum + n[k] + i] = 1 / (1 + exp(-net));
 		}
-		out[n[0] + i] = 1 / (1 + exp(-net));
+		Wnum = Wnum + n[k] * n[k + 1];
+		Onum = Onum + n[k];
 	}
 
-	for (int i = 0; i < n[2]; i++) {
-		double net = 0;
-		for (int j = 0; j < n[1]; j++) {
-			net = weight[n[0] * n[1] + i * n[1] + j] * out[n[0] + j] + net;
-		}
-		out[n[0] + n[1] + i] = 1 / (1 + exp(-net));
-	}
 }
 
-void DisIter(double* del, double* outO, double* out, double* weight, double* delw, int* n, double iter) {
-	int coat = 3;
-	int Dnum = 0, Onum = 0, Wnum = 0;
+void DisIter(float* del, float* outO, float* out, float* weight, float* delw, int* n, float iter, int layer, int NeuralSum, int WeightSum) {
+	int Dnum = 0, Onum = NeuralSum - n[layer - 1], Wnum = WeightSum;
 
 	for (int i = 0; i < n[2]; i++) {
 		del[i] = (outO[i] - out[n[0] + n[1] + i]) * out[n[0] + n[1] + i] * (1 - out[n[0] + n[1] + i]) * iter;
 	}
-	Dnum = n[2];
-	Wnum = n[0] * n[1];
 
-	for (int i = 0; i < n[coat - 2]; i++) {
-		double per = 0;
-		for (int j = 0; j < n[coat - 1]; j++) {
-			per = per + del[j] * weight[Wnum + i + n[coat - 2] * j];
+	for (int k = 1; k < (layer - 1); k++) {
+		Onum = Onum - n[layer - k - 1];
+		Wnum = Wnum - n[layer - k - 1] * n[layer - k];
+		for (int i = 0; i < n[layer - k - 1]; i++) {
+			float per = 0;
+			for (int j = 0; j < n[layer - k]; j++) {
+				per = per + del[Dnum + j] * weight[Wnum + i + n[layer - k - 1] * j];
+			}
+			del[Dnum + n[layer - k] + i] = (1 - out[Onum + i]) * out[Onum + i] * per;
 		}
-		del[Dnum + i] = (1 - out[n[0] + i]) * out[n[0] + i] * per;
+		Dnum = Dnum + n[layer - k];
 	}
 
 	Dnum = 0;
@@ -71,11 +54,11 @@ void DisIter(double* del, double* outO, double* out, double* weight, double* del
 	Wnum = n[0] * n[1];
 
 	for (int i = 0; i < n[1]; i++) {
-		double grad = 0;
+		float grad = 0;
 		for (int j = 0; j < n[2]; j++) {
 			grad = del[Dnum + j] * out[Onum + i];
-			delw[Wnum + i + n[coat - 2] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[coat - 2] * j];
-			weight[Wnum + i + n[coat - 2] * j] = weight[Wnum + i + n[coat - 2] * j] + delw[Wnum + i + n[coat - 2] * j];
+			delw[Wnum + i + n[layer - 2] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[layer - 2] * j];
+			weight[Wnum + i + n[layer - 2] * j] = weight[Wnum + i + n[layer - 2] * j] + delw[Wnum + i + n[layer - 2] * j];
 		}
 	}
 
@@ -84,27 +67,27 @@ void DisIter(double* del, double* outO, double* out, double* weight, double* del
 	Wnum = 0;
 
 	for (int i = 0; i < n[0]; i++) {
-		double grad = 0;
+		float grad = 0;
 		for (int j = 0; j < n[1]; j++) {
 			grad = del[Dnum + j] * out[Onum + i];
-			delw[Wnum + i + n[coat - 3] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[coat - 3] * j];
-			weight[Wnum + i + n[coat - 3] * j] = weight[Wnum + i + n[coat - 3] * j] + delw[Wnum + i + n[coat - 3] * j];
+			delw[Wnum + i + n[layer - 3] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[layer - 3] * j];
+			weight[Wnum + i + n[layer - 3] * j] = weight[Wnum + i + n[layer - 3] * j] + delw[Wnum + i + n[layer - 3] * j];
 		}
 	}
 }
 
-void GenIter(double* del, double* outO, double* out, double* weight, double* delw, int* n, int* var, int ind, double k, double iter) {
-	int coat = 3;
+void GenIter(float* del, float* outO, float* out, float* weight, float* delw, int* n, int* var, int ind, float k, float iter) {
+	int layer = 3;
 	int Dnum = 0, Onum = 0, Wnum = 0;
 
-	del[ind] = -log(outO[0]) * k * iter /** (1 - out[n[0] + n[1] + ind]) * out[n[0] + n[1] + ind]*/;
+	del[ind] = -log(outO[0]) * k * iter * (1 - out[n[0] + n[1] + ind]) * out[n[0] + n[1] + ind];
 
 	Dnum = n[2];
 	Wnum = n[0] * n[1];
 
-	for (int i = 0; i < n[coat - 2]; i++) {
-		double per = 0;
-		per = per + del[ind] * weight[Wnum + i + n[coat - 2] * ind];
+	for (int i = 0; i < n[layer - 2]; i++) {
+		float per = 0;
+		per = per + del[ind] * weight[Wnum + i + n[layer - 2] * ind];
 		del[Dnum + i] = (1 - out[n[0] + i]) * out[n[0] + i] * per;
 	}
 
@@ -113,7 +96,7 @@ void GenIter(double* del, double* outO, double* out, double* weight, double* del
 	Wnum = n[0] * n[1];
 
 	for (int i = 0; i < n[1]; i++) {
-		double grad = 0;
+		float grad = 0;
 		grad = del[Dnum + ind] * out[Onum + i];
 		//std::cout << grad << std::endl;
 		delw[Wnum + i + n[1] * ind] = 0.5 * grad + 0.3 * delw[Wnum + i + n[1] * ind];
@@ -125,27 +108,26 @@ void GenIter(double* del, double* outO, double* out, double* weight, double* del
 	Wnum = 0;
 
 	for (int i = 0; i < n[0]; i++) {
-		double grad = 0;
+		float grad = 0;
 		for (int j = 0; j < n[1]; j++) {
 			grad = del[Dnum + j] * out[Onum + i];
-			delw[Wnum + i + n[coat - 3] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[coat - 3] * j];
-			weight[Wnum + i + n[coat - 3] * j] = weight[Wnum + i + n[coat - 3] * j] + delw[Wnum + i + n[coat - 3] * j];
+			delw[Wnum + i + n[layer - 3] * j] = 0.5 * grad + 0.3 * delw[Wnum + i + n[layer - 3] * j];
+			weight[Wnum + i + n[layer - 3] * j] = weight[Wnum + i + n[layer - 3] * j] + delw[Wnum + i + n[layer - 3] * j];
 		}
 	}
 }
 
 
-void Random(double* out, int n) {
+void Random(float* out, int n) {
 	srand(static_cast<unsigned int>(clock()));
 	for (int i = 0; i < n; i++) {
-		double per;
-		per = (double)(rand()) / RAND_MAX * 2 - 1;
-		out[i] = (double)(rand()) / RAND_MAX;
-		//cout << out[i] << endl;
+		float per;
+		per = (float)(rand()) / RAND_MAX * 2 - 1;
+		out[i] = (float)(rand()) / RAND_MAX;
 	}
 }
 
-void Out(double* out, int* n) {
+void Out(float* out, int* n) {
 	for (int i = 0; i < n[2]; i++) {
 		std::cout << out[n[0] + n[1] + i] << std::endl;
 	}
