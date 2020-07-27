479a81aa4059e7af56e5b3aa6b55ba64b045a22a
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-28 00:58:14 +0300

    fix

--- experimental/struct/gan.cpp
+++ experimental/struct/gan.cpp
@@ -3,19 +3,20 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <ctime>
 //#include "Header.h"
 
 const int GeneratorLayer = 3;
 
 class Generator {
 private:
-    int InitializationNeuronSum() {
+    int CreateNeuronSum() {
         int neuron = 0;
         for (int i = 0; i < GeneratorLayer; i++)
             neuron = neuron + Generator::coat[i];
         return neuron;
     }
-    int InitializationWeightSum() {
+    int CreateWeightSum() {
         int weight = 0;
         for (int i = 0; i < (GeneratorLayer - 1); i++)
             weight = weight + Generator::coat[i] * Generator::coat[i + 1];
@@ -23,7 +24,7 @@ private:
     }
 
 public:
-    int WeightSum = 0, NeuronSum = 0;
+    int WeightSum, NeuronSum;
     int* coat = new int[GeneratorLayer];
 
     void CreateCoat(int data, ...) {
@@ -33,27 +34,32 @@ public:
             p++;
             p++;
         }
-        Generator::NeuronSum = InitializationNeuronSum();
-        Generator::WeightSum = InitializationWeightSum();
+        Generator::WeightSum = CreateWeightSum();
+        Generator::NeuronSum = CreateNeuronSum();
     }
 
+    /*fix this*/
     float* weight = new float[WeightSum];
     float* delw = new float[WeightSum];
     float* out = new float[NeuronSum];
 
-    void Initialization() {
+    void Initialization(float max, float min) {
+        srand(static_cast<unsigned int>(clock()));
         for (int i = 0; i < Generator::WeightSum; i++) {
-            Generator::weight[i] = 0;
-            Generator::delw[i] = 0;
+            Generator::weight[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
+            Generator::delw[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
         }
     }
 }Generator;
 
 
 int main() {
-    Generator.CreateCoat(4, 3, 5);
-    Generator.Initialization();
-
+    Generator.CreateCoat(4, 30, 5);
     std::cout << Generator.NeuronSum << std::endl;
     std::cout << Generator.WeightSum << std::endl;
+    Generator.Initialization(3, -3);
+
+    for (int i = 0; i < Generator.WeightSum; i++)
+        std::cout << Generator.weight[i] << std::endl;
+
 }
