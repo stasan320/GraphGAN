362bc6e8bbb6e6f7fdcb545dbfd0a1146b7fe205
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-27 13:41:13 +0300

    Update gan.cpp

--- experimental/struct/gan.cpp
+++ experimental/struct/gan.cpp
@@ -9,28 +9,34 @@ const int GeneratorLayer = 3;
 
 class Generator {
 private:
-
+    
 public:
-    //float* weight = new float[];
-    //std::vector<int> coat;
+    int WeightSum = 0, NeuronSum = 0;
     int* coat = new int[GeneratorLayer];
-    void InitializationCoat(int* Gcoat, int Layer, int coat, ...) {
+    //std::vector<int> coat;
+    void InitializationCoat(int* coat, int Layer, int data, ...) {
         int sum = 0;
         int testsum = 0;
-        int* p = &coat;
+        int* p = &data;
         for (int i = 0; i < Layer; i++) {
             //std::cout << *p << std::endl;
-            Gcoat[i] = *p;
+            coat[i] = *p;
             p++;
-            p++;  //i dnt know, but its working
+            p++;
         }
+    }
+    void InitializationWeight(int Layer, int WeightSum, int NeuronSum, int* coat) {
+        for (int i = 0; i < Layer; i++)
+            NeuronSum = NeuronSum + coat[i];
+        std::cout << NeuronSum << std::endl;
     }
 }Generator;
 
 
 int main() {
-    Generator.InitializationCoat(Generator.coat, GeneratorLayer, 4, 3, 5);
+    Generator.InitializationCoat(Generator.coat, GeneratorLayer, 4, 30, 5);
+    Generator.InitializationWeight(GeneratorLayer, Generator.WeightSum, Generator.NeuronSum, Generator.coat);
 
-    for (int i = 0; i < GeneratorLayer; i++)
-        std::cout << Generator.coat[i] << std::endl;
+    /*for (int i = 0; i < GeneratorLayer; i++)
+        std::cout << Generator.coat[i] << std::endl;*/
 }
