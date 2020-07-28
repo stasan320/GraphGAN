11f4aa3c0f5577b72024a0d05d6e1c4407c2af1f
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-07-28 22:26:44 +0300

    Update gan.cpp

--- experimental/struct/gan.cpp
+++ experimental/struct/gan.cpp
@@ -22,9 +22,26 @@ private:
             weight = weight + Generator::coat[i] * Generator::coat[i + 1];
         return weight;
     }
+    void SumFunc(float* weight, float* out) {
+        int Wnum = 0, Onum = 0;
+        for (int i = 0; i < (GeneratorLayer - 1); i++) {
+            for (int j = 0; j < Generator::coat[i + 1]; j++) {
+                float net = 0;
+                for (int k = 0; k < Generator::coat[i]; k++) {
+                    net = net + weight[Wnum + j * Generator::coat[i] + k] * out[Onum + k];
+                }
+                out[Onum + Generator::coat[i] + j] = 1 / (1 + exp(-net));
+                //std::cout << out[Onum + Generator::coat[i] + j] << std::endl;
+            }
+            Wnum = Wnum + Generator::coat[i] * Generator::coat[i + 1];
+            Onum = Onum + Generator::coat[i];
+        }
+    }
+    void Delta(float* weight, float* out, float* del) {
 
+    }
 public:
-    int WeightSum, NeuronSum;
+    int WeightSum = 0, NeuronSum = 0;
     int* coat = new int[GeneratorLayer];
 
     void CreateCoat(int data, ...) {
@@ -38,28 +55,27 @@ public:
         Generator::NeuronSum = CreateNeuronSum();
     }
 
-    /*fix this*/
-    float* weight = new float[WeightSum];
-    float* delw = new float[WeightSum];
-    float* out = new float[NeuronSum];
-
-    void Initialization(float max, float min) {
+    void Initialization(float* weight, float* delw, float max, float min) {
         srand(static_cast<unsigned int>(clock()));
         for (int i = 0; i < Generator::WeightSum; i++) {
-            Generator::weight[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
-            Generator::delw[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
+            weight[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
+            delw[i] = 0;
         }
+    }
+    void Iteration(float* weight, float* out) {
+        SumFunc(weight, out);
     }
 }Generator;
 
 
 int main() {
-    Generator.CreateCoat(4, 30, 5);
-    std::cout << Generator.NeuronSum << std::endl;
-    std::cout << Generator.WeightSum << std::endl;
-    Generator.Initialization(3, -3);
-
-    for (int i = 0; i < Generator.WeightSum; i++)
-        std::cout << Generator.weight[i] << std::endl;
-
+    Generator.CreateCoat(2, 6, 1);
+    float* weight = new float[Generator.WeightSum];
+    float* delw = new float[Generator.WeightSum];
+    float* out = new float[Generator.NeuronSum];
+    float* del = new float[Generator.NeuronSum - Generator.coat[0]];
+    out[0] = 1;
+    out[1] = 0;
+    Generator.Initialization(weight, delw, 3, -3);
+    Generator.Iteration(weight, out);
 }
