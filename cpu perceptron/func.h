5c983f3b7a09a69dcba41b2e625891eb56198b2c
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-08-10 20:41:32 +0300

    big fix

--- cpu perceptron/func.h
+++ cpu perceptron/func.h
@@ -1,5 +1,5 @@
-void Random(float* Arr, float min, float max, int start, int end, unsigned long int time) {
-	srand(static_cast<unsigned int>(time));
+void Random(float* Arr, float min, float max, int start, int end, unsigned long long time) {
+	srand(static_cast<unsigned long long>(time));
 	for (int i = start; i < end; i++) {
 		Arr[i] = (float)(rand()) / RAND_MAX * (max - min) + min;
 	}
@@ -21,8 +21,32 @@ void SumFunc(float* out, float* weight, int* n, int num) {
 	}
 }
 
-void IterNull(float* out, float* outO, float* weight, float* delw, float* del, int* n, int cout) {
+void GenIterNull(float* out, float outO, float* weight, float* delw, float* del, int* n, int cout, int index, float dop) {
 	int Onum = 0, Wnum = 0;
+
+	for (int i = 0; i < (cout - 1); i++) {
+		Onum = Onum + n[i];
+	}
+
+	del[index] = -log(outO) * dop;
+
+	Onum = 0;
+
+	for (int i = 0; i < (cout - 2); i++) {
+		Onum = Onum + n[i];
+		Wnum = Wnum + n[i] * n[i + 1];
+	}
+
+	for (int i = 0; i < n[cout - 2]; i++) {
+		float grad = out[Onum + i] * del[index];
+		delw[Wnum + i + index * n[cout - 2]] = 0.5 * grad + 0.03 * delw[Wnum + i + index * n[cout - 2]];
+		weight[Wnum + i + index * n[cout - 2]] = weight[Wnum + i + index * n[cout - 2]] + delw[Wnum + i + index * n[cout - 2]];
+
+	}
+}
+
+void DisIterNull(float* out, float* outO, float* weight, float* delw, float* del, int* n, int cout) {
+	int Onum = 0, Wnum = 0, Dnum = 0;
 
 	for (int i = 0; i < (cout - 1); i++) {
 		Onum = Onum + n[i];
@@ -38,10 +62,11 @@ void IterNull(float* out, float* outO, float* weight, float* delw, float* del, i
 		Onum = Onum + n[i];
 		Wnum = Wnum + n[i] * n[i + 1];
 	}
+	//std::cout << Wnum << std::endl;
 
 	for (int i = 0; i < n[cout - 2]; i++) {
 		for (int j = 0; j < n[cout - 1]; j++) {
-			float grad = out[Onum + i] * del[j];
+			float grad = out[Onum + i] * del[Dnum + j];
 			delw[Wnum + i + j * n[cout - 2]] = 0.5 * grad + 0.03 * delw[Wnum + i + j * n[cout - 2]];
 			weight[Wnum + i + j * n[cout - 2]] = weight[Wnum + i + j * n[cout - 2]] + delw[Wnum + i + j * n[cout - 2]];
 		}
@@ -50,28 +75,29 @@ void IterNull(float* out, float* outO, float* weight, float* delw, float* del, i
 
 void Iter(float* out, float* weight, float* delw, float* del, int* n, int num, int coat) {
 	int Onum = 0, Wnum = 0, Dnum = 0;
-	for (int i = 0; i < (num - 1); i++) {
+	for (int i = 0; i < (coat - num - 3); i++) {
 		Onum = Onum + n[i];
 		Wnum = Wnum + n[i] * n[i + 1];
 	}
 
-	for (int i = (num + 1); i < coat; i++) {
+	for (int i = (coat - 1); i > (coat - num - 2); i--) {
 		Dnum = Dnum + n[i];
 	}
+	//std::cout << Dnum << std::endl;
 
-	for (int i = 0; i < n[num - 1]; i++) {
+	for (int i = 0; i < n[coat - num - 3]; i++) {
 		float per = 0;
-		for (int j = 0; j < n[num]; j++) {
-			per = per + del[Dnum + i] * weight[Wnum + i + n[num - 1] * j];
+		for (int j = 0; j < n[coat - num - 2]; j++) {
+			per = per + del[Dnum + i] * weight[Wnum + i + n[coat - num - 2] * j];
 		}
-		del[Dnum + n[num] + i] = (1 - out[Onum + i]) * out[Onum + i] * per;
+		del[Dnum + n[coat - num - 2] + i] = (1 - out[Onum + i]) * out[Onum + i] * per;
 	}
 
-	for (int i = 0; i < n[num - 1]; i++) {
-		for (int j = 0; j < n[num]; j++) {
-			float grad = out[Onum + i] * del[j];
-			delw[Wnum + i + j * n[num - 2]] = 0.5 * grad + 0.03 * delw[Wnum + i + j * n[num - 2]];
-			weight[Wnum + i + j * n[num - 2]] = weight[Wnum + i + j * n[num - 2]] + delw[Wnum + i + j * n[num - 2]];
+	for (int i = 0; i < n[coat - num - 3]; i++) {
+		for (int j = 0; j < n[coat - num - 2]; j++) {
+			float grad = out[Onum + i] * del[Dnum + j];
+			delw[Wnum + i + j * n[coat - num - 3]] = 0.5 * grad + 0.03 * delw[Wnum + i + j * n[coat - num - 3]];
+			weight[Wnum + i + j * n[coat - num - 3]] = weight[Wnum + i + j * n[coat - num - 3]] + delw[Wnum + i + j * n[coat - num - 3]];
 		}
 	}
 }
@@ -87,12 +113,12 @@ void Image(cv::Mat image, float* out, int Onum) {
 	}
 }
 
-void Out(cv::Mat result, float* out, int Onum) {
-	for (int i = 0; i < result.rows; i++) {
-		for (int j = 0; j < result.cols; j++) {
-			result.at<uchar>(i, j) = ceil(out[Onum + j + i * result.cols] * 255);
+void Out(cv::Mat image, float* out, int Onum) {
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			image.at<uchar>(i, j) = ceil(out[Onum + j + i * image.cols] * 255);
 		}
 	}
-	cv::imshow("Out", result);
+	cv::imshow("Out", image);
 	cv::waitKey(1);
 }
