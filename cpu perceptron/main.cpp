52e04f9d033e44ad60f5cc3433f76a920c469f35
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-08-11 20:08:30 +0300

    Update main.cpp

--- cpu perceptron/main.cpp
+++ cpu perceptron/main.cpp
@@ -1,15 +1,5 @@
-//Create by Stasan
-//nan-error ? вероятно из-за нарушения работы loss function
-
-//1, 2 - 784, 32, 4, 2						//
-//3, 4 - 784, 64, 16, 3						//sigm
-//5, 6, 7, 8 - 784, 128, 16, 5				//sigm
-//9 ?										//
-
-//2 - 784, 32, 4, 2                         //tang, в основном работает, иногда вылетает nan, испр проверку на NaN
-
-
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <ctime>
 #include <opencv2/opencv.hpp>
@@ -17,11 +7,12 @@
 
 #include "Header.h"
 
-const int coat = 4;
+const int coat = 5;
 
 int main() {
 	cv::Mat result(28, 28, CV_8UC1);
-	int n[coat] = { 784, 32, 4, 2 }, Onum = 0, Dnum = 0, Wnum = 0;
+	cv::Mat image(28, 28, CV_8UC3);
+	int n[coat] = { 784, 512, 128, 64, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
 	int t = clock();
 
 	for (int i = 0; i < coat; i++) {
@@ -31,42 +22,41 @@ int main() {
 	for (int i = 0; i < (coat - 1); i++) {
 		Wnum = Wnum + n[i] * n[i + 1];
 	}
+	
+	double* out = new double[Onum];
+	double* outO = new double[n[coat - 1]];
+	double* del = new double[Onum];
+	double* weight = new double[Wnum];
+	double* delw = new double[Wnum];
 
-	float* out = new float[Onum];
-	float* outO = new float[n[coat - 1]];
-	float* del = new float[Onum];
-	float* weight = new float[Wnum];
-	float* delw = new float[Wnum];
-
-	Random(weight, -1, 1, 0, Wnum, clock());
+	Random(weight, -3, 3, 0, Wnum, clock());
 	for (int i = 0; i < Wnum; i++) {
 		delw[i] = 0;
 	}
-	for (int i = 0; i < n[coat - 1]; i++)
+	for (int i = 0; i < n[coat - 1]; i++) {
 		outO[i] = 0;
-
-	for (int epoch = 0; epoch < 6000; epoch++) {
+	}
+	std::cout << "Start" << std::endl;
+	int num;
+	std::cout << "Enter the number of epochs ";
+	std::cin >> num;
+	for (unsigned int epoch = 0; epoch < num; epoch++) {
 		for (int k = 0; k < n[coat - 1]; k++) {
-			cv::Mat image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 100 + 1) + ").png");
+			image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png");
 			if (!image.data) {
 				std::cout << "Error upload image";
 				return -1;
 			}
-			cv::imshow("Out", image);
-			cv::waitKey(1);
 			Image(image, out, 0);
 			for (int i = 0; i < (coat - 1); i++) {
 				SumFunc(out, weight, n, i);
-				//std::cout << std::endl;
 			}
-			//return 0;
-			//Out(result, out, Onum - n[coat - 1]);
+
 			for (int i = 0; i < n[coat - 1]; i++) {
-				//std::cout << out[Onum - n[coat - 1] + i] << std::endl;
-			}
-			if (out[Onum - n[coat - 1]] == NAN) {
-				std::cout << "NaN error";
-				return 2;
+				if (out[Onum - n[coat - 1]] != out[Onum - n[coat - 1]]) {
+					std::cout << "NaN error";
+					return 2;
+				}
 			}
 
 			outO[k] = 1;
@@ -74,17 +64,25 @@ int main() {
 			for (int i = 0; i < (coat - 2); i++) {
 				Iter(out, weight, delw, del, n, i, coat);
 			}
-			//return 0;
-			outO[k] = -1;
-			//std::cout << std::endl;
+			outO[k] = 0;
+		}
+		if (epoch % 50 == 0) {
+			double error = 0;
+			for (int i = 0; i < n[coat - 1]; i++) {
+				error = error + (outO[i] - out[Onum - n[coat - 1] + i]) * (outO[i] - out[Onum - n[coat - 1] + i]) / n[coat - 1];
+			}
+			std::cout << "Epoch[" << epoch << "][" << error << "] \r";
 		}
 	}
+	std::cout << std::endl;
+	std::cout << "Past your path" << std::endl;
+
 	for (;;) {
 		std::string name;
 		getline(std::cin, name);
 		cv::Mat image = cv::imread(name);
 		if (!image.data) {
-			std::cout << "Error upload image";
+			std::cout << "Error upload image" << std::endl;
 			continue;
 		}
 		cv::imshow("Out", image);
@@ -92,13 +90,24 @@ int main() {
 		Image(image, out, 0);
 		for (int i = 0; i < (coat - 1); i++) {
 			SumFunc(out, weight, n, i);
-			//std::cout << std::endl;
 		}
-		//return 0;
-		//Out(result, out, Onum - n[coat - 1]);
 		for (int i = 0; i < n[coat - 1]; i++) {
-			std::cout << out[Onum - n[coat - 1] + i] << std::endl;
+			if (out[Onum - n[coat - 1]] != out[Onum - n[coat - 1]]) {
+				std::cout << "NaN error";
+				return 2;
+			}
 		}
+
+		double data = 0;
+		int number = 0;
+		for (int i = 0; i < n[coat - 1]; i++) {
+			if (data < out[Onum - n[coat - 1] + i]) {
+				data = out[Onum - n[coat - 1] + i];
+				number = i;
+			}
+			std::cout << i << " - " << out[Onum - n[coat - 1] + i] << std::endl;
+		}
+		std::cout << "It's " << number << std::endl;
 	}
 	return 0;
 }
