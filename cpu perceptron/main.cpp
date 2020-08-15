bd7a8d5d6e639e37ba484a85cc6c2da402ccafcb
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-08-15 20:31:16 +0300

    update error, console time

--- cpu perceptron/main.cpp
+++ cpu perceptron/main.cpp
@@ -1,4 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <iomanip>
+#include <vector>
 #include <cmath>
 #include <ctime>
 #include <opencv2/opencv.hpp>
@@ -6,12 +10,14 @@
 
 #include "Header.h"
 
+
 const int coat = 5;
 
 int main() {
 	cv::Mat result(28, 28, CV_8UC1);
 	cv::Mat image(28, 28, CV_8UC3);
-	int n[coat] = { 784, 512, 128, 64, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
+	cv::Mat errors(100, 300, CV_8UC1);
+	int n[coat] = { 784, 512, 128, 32, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
 	int t = clock();
 
 	for (int i = 0; i < coat; i++) {
@@ -27,6 +33,7 @@ int main() {
 	double* del = new double[Onum];
 	double* weight = new double[Wnum];
 	double* delw = new double[Wnum];
+	std::vector<double> ErrorOut;
 
 	Random(weight, -3, 3, 0, Wnum, clock());
 	for (int i = 0; i < Wnum; i++) {
@@ -35,27 +42,18 @@ int main() {
 	for (int i = 0; i < n[coat - 1]; i++) {
 		outO[i] = 0;
 	}
-	std::cout << "Start" << std::endl;
+
 	int num;
-	std::cout << "Enter the number of epochs ";
+	std::cout << "Enter the number of epochs: ";
 	std::cin >> num;
+	double error = 0;
+	
 	for (unsigned int epoch = 0; epoch < num; epoch++) {
 		for (int k = 0; k < n[coat - 1]; k++) {
 			image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png");
-			if (!image.data) {
-				std::cout << "Error upload image";
-				return -1;
-			}
 			Image(image, out, 0);
 			for (int i = 0; i < (coat - 1); i++) {
 				SumFunc(out, weight, n, i);
-			}
-
-			for (int i = 0; i < n[coat - 1]; i++) {
-				if (out[Onum - n[coat - 1]] != out[Onum - n[coat - 1]]) {
-					std::cout << "NaN error";
-					return 2;
-				}
 			}
 
 			outO[k] = 1;
@@ -63,14 +61,59 @@ int main() {
 			for (int i = 0; i < (coat - 2); i++) {
 				Iter(out, weight, delw, del, n, i, coat);
 			}
+			
+			if (epoch % 17 == 0) {
+				error = 0;
+				for (int i = 0; i < n[coat - 1]; i++) {
+					error = error + (outO[i] - out[Onum - n[coat - 1] + i]) * (outO[i] - out[Onum - n[coat - 1] + i]);
+				}
+				error = error / n[coat - 1];
+
+				ErrorOut.push_back(error);
+				for (int i = 0; i < errors.rows; i++) {
+					for (int j = 0; j < errors.cols; j++) {
+						errors.at<uchar>(i, j) = 0;
+					}
+				}
+
+				for (int i = 0; i < ErrorOut.size(); i++) {
+					int dat = errors.rows - ErrorOut[i] * errors.rows;
+					float min = (i * (errors.cols - 1) / (float)ErrorOut.size());
+					float max = (i + 1) * (errors.cols - 1) / (float)ErrorOut.size();
+
+					for (float j = min; j <= max; j = j + 1) {
+						errors.at<uchar>(dat, round(j)) = 255;
+					}
+				}
+				cv::imshow("Error", errors);
+				cv::waitKey(1);
+			}
 			outO[k] = 0;
 		}
-		if (epoch % 50 == 0) {
-			double error = 0;
+
+		if (time(NULL) > t) {
+			std::string sec = "", min = "", hour = "";
+			t = time(NULL);
 			for (int i = 0; i < n[coat - 1]; i++) {
-				error = error + (outO[i] - out[Onum - n[coat - 1] + i]) * (outO[i] - out[Onum - n[coat - 1] + i]) / n[coat - 1];
+				if (out[Onum - n[coat - 1] + i] != out[Onum - n[coat - 1] + i]) {
+					std::cout << "NaN error";
+					return 2;
+				}
 			}
-			std::cout << "Epoch[" << epoch << "][" << error << "] \r";
+
+			time_t now = time(NULL);
+			tm* ltm = localtime(&now);
+			std::cout << "                                                  \r";
+			if (ltm->tm_sec < 10) {
+				sec = "0";
+			}
+			if (ltm->tm_min < 10) {
+				min= "0";
+			}
+			if (ltm->tm_hour < 10) {
+				hour = "0";
+			}
+			std::cout << "[" << hour << ltm->tm_hour << ":" << min << ltm->tm_min << ":" << sec << ltm->tm_sec << "][" << epoch << "][" << std::setprecision(5) << error << "] \r";
 		}
 	}
 	std::cout << std::endl;
@@ -78,7 +121,7 @@ int main() {
 
 	for (;;) {
 		std::string name;
-		getline(std::cin, name);
+		std::cin >> name;
 		cv::Mat image = cv::imread(name);
 		if (!image.data) {
 			std::cout << "Error upload image" << std::endl;
