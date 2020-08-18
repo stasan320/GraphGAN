4607261b4ecb584dbc446e6ca7f9569da11fbbbb
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-08-18 15:55:38 +0300

    Update main.cpp

--- cpu perceptron/main.cpp
+++ cpu perceptron/main.cpp
@@ -17,7 +17,7 @@ int main() {
 	cv::Mat result(28, 28, CV_8UC1);
 	cv::Mat image(28, 28, CV_8UC3);
 	cv::Mat errors(100, 300, CV_8UC1);
-	int n[coat] = { 784, 30, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
+	int n[coat] = { 784, 55, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
 	int t = clock();
 
 	for (int i = 0; i < coat; i++) {
@@ -35,7 +35,7 @@ int main() {
 	double* delw = new double[Wnum];
 	std::vector<double> ErrorOut;
 
-	Random(weight, -3.0, 3.0, 0, Wnum, clock());
+	Random(weight, -1.0, 1.0, 0, Wnum, clock());
 	for (int i = 0; i < Wnum; i++) {
 		delw[i] = 0;
 	}
@@ -50,9 +50,9 @@ int main() {
 	
 	for (unsigned int epoch = 0; epoch < num; epoch++) {
 		for (int k = 0; k < n[coat - 1]; k++) {
-			image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 3000 + 1) + ").png");
+			image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png");
 			if (!image.data) {
-				std::cout << "Error upload image " << "D:\\Foton\\ngnl_data\\testing\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 3000 + 1) + ").png";
+				std::cout << "Error upload image " << "D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png";
 				return -1;
 			}
 			Image(image, out, 0);
@@ -79,7 +79,7 @@ int main() {
 			error = error / n[coat - 1];
 			ErrorOut.push_back(error);
 
-			if (epoch % 107 == 0) {
+			if (epoch % 1007 == 0) {
 				for (int i = 0; i < errors.rows; i++) {
 					for (int j = 0; j < errors.cols; j++) {
 						errors.at<uchar>(i, j) = 0;
@@ -95,13 +95,14 @@ int main() {
 					float min = (i * (errors.cols - 1) / (float)ErrorOut.size());
 					float max = (i + 1) * (errors.cols - 1) / (float)ErrorOut.size();
 
-					for (float j = min; j <= max; j = j + 1) {
+					for (float j = min; j < max; j = j + 1) {
 						errors.at<uchar>(round(dat), round(j)) = 255;
 					}
 				}
 				cv::imshow("Error", errors);
 				cv::waitKey(1);
 			}
+			//return 0;
 			outO[k] = 0.0;
 		}
 
@@ -129,44 +130,48 @@ int main() {
 			}
 			std::cout << "[" << hour << ltm->tm_hour << ":" << min << ltm->tm_min << ":" << sec << ltm->tm_sec << "][" << epoch << "][" << std::setprecision(5) << ErrorOut[ErrorOut.size() - epoch % 7] << "] \r";
 		}
-	}
-	std::cout << std::endl;
-	std::cout << "Testing" << std::endl;
 
-	std::vector<int>DErrors(10, 0);
-	for (int k = 0; k < n[coat - 1]; k++) {
-		for (int test = 0; test < 890; test++) {
-			image = cv::imread("D:\\Foton\\ngnl_data\\testing\\" + std::to_string(k) + "\\1 (" + std::to_string(test + 1) + ").png");
-			if (!image.data) {
-				std::cout << "Error upload image " << "D:\\Foton\\ngnl_data\\testing\\" + std::to_string(k) + "\\1 (" + std::to_string(test + 1) + ").png";
-				return -1;
-			}
-			Image(image, out, 0);
-			for (int i = 0; i < (coat - 1); i++) {
-				SumFunc(out, weight, n, i);
-				//std::cout << std::endl;
-			}
+		if (t == 0) {
+			std::cout << std::endl;
+			std::cout << "Testing" << std::endl;
 
-			double data = 0;
-			int number = 0;
-			for (int i = 0; i < n[coat - 1]; i++) {
-				if (data < out[Onum - n[coat - 1] + i]) {
-					data = out[Onum - n[coat - 1] + i];
-					number = i;
+			std::vector<int>DErrors(10, 0);
+			for (int k = 0; k < n[coat - 1]; k++) {
+				for (int test = 0; test < 890; test++) {
+					image = cv::imread("D:\\Foton\\ngnl_data\\testing\\" + std::to_string(k) + "\\1 (" + std::to_string(test + 1) + ").png");
+					if (!image.data) {
+						std::cout << "Error upload image " << "D:\\Foton\\ngnl_data\\testing\\" + std::to_string(k) + "\\1 (" + std::to_string(test + 1) + ").png";
+						return -1;
+					}
+					Image(image, out, 0);
+					for (int i = 0; i < (coat - 1); i++) {
+						SumFunc(out, weight, n, i);
+					}
+
+					double data = 0;
+					int number = 0;
+					for (int i = 0; i < n[coat - 1]; i++) {
+						if (data < out[Onum - n[coat - 1] + i]) {
+							data = out[Onum - n[coat - 1] + i];
+							number = i;
+						}
+					}
+					if (k == number) {
+						DErrors[k] = DErrors[k] + 1;
+					}
 				}
+				std::cout << "Result for " << k << ": " << std::setprecision(5) << (double)DErrors[k] / (double)900 << std::endl;
 			}
-			if (k == number) {
-				DErrors[k] = DErrors[k] + 1;
+			double data = 0;
+			for (int i = 0; i < n[coat - 1]; i++) {
+				data = DErrors[i] + data;
 			}
+			data = data / (900 * (double)n[coat - 1]);
+			std::cout << "Result for all: " << std::setprecision(5) << data << std::endl;
 		}
-		std::cout << "Result for " << k << ": " << std::setprecision(5) << (double)DErrors[k] / (double)900 << std::endl;
 	}
-	double data = 0;
-	for (int i = 0; i < n[coat - 1]; i++) {
-		data = DErrors[i] + data;
-	}
-	data = data / (900 * (double)n[coat - 1]);
-	std::cout << "Result for all: " << std::setprecision(5) << data << std::endl;
+
+	Testing(image, out, weight, coat, n);
 
 	std::cout << std::endl;
 	std::cout << "Past your path" << std::endl;
