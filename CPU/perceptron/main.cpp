0fa5b465f3c77643cc0e279fa642c0ab28bf3152
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-11-24 20:32:47 +0300

    Update main.cpp

--- CPU/perceptron/main.cpp
+++ CPU/perceptron/main.cpp
@@ -16,6 +16,7 @@
 #include "FunctionNeural.h"
 #include "FunctionConsole.h"
 #include "FunctionTesting.h"
+#include "OldFunc.h"
 
 const int coat = 3;
 
@@ -24,16 +25,15 @@ int main() {
 	cv::Mat image(28, 28, CV_8UC3);
 	cv::Mat errors(100, 300, CV_8UC1);
 	int n[coat] = { 784, 30, 10 }, Onum = 0, Dnum = 0, Wnum = 0;
-	double step = 0.35;
+	float step = 0.36;
 	std::string path, ProgramData[2];
-	//std::string 11nfigurationPath;
 	path = ConfigPath();
 	//std::cout << Path;
 	//return 0;
 	//std::string ConfigurationPath(const std::wstring & Path);
-	std::ifstream Configuration(path + "\\config");
+	/*std::ifstream Configuration(path + "\\config");
 	ProgramConst(ProgramData);
-	step = std::stod(ProgramData[1]);
+	step = std::stod(ProgramData[1]);*/
 
 	/*while(Configuration) {
 		Configuration >> backup_weight;
@@ -47,25 +47,26 @@ int main() {
 		Wnum = Wnum + n[i] * n[i + 1];
 	}
 
-	double* out = new double[Onum];
-	double* outO = new double[n[coat - 1]];
-	double* del = new double[Onum - n[coat - 1]];
-	double* weight = new double[Wnum];
-	double* delw = new double[Wnum];
-	std::vector<double> ErrorOut;
+	float* out = new float[Onum];
+	float* outO = new float[n[coat - 1]];
+	float* del = new float[Onum - n[(int)coat - (int)1]];
+	float* weight = new float[Wnum];
+	float* delw = new float[Wnum];
+	std::vector<float> ErrorOut;
 
-	if (ProgramData[0] == "0") {
+	Random(weight, -1, 1, 0, Wnum, clock());
+	/*if (ProgramData[0] == "0") {
 		Random(weight, -1, 1, 0, Wnum, clock());
 	}
 	else {
-		std::ifstream Tweight(path + "\\weight"/*, std::ios::binary | std::ios::out*/);
+		std::ifstream Tweight(path + "\\weight");
 		//std::ofstream w(path + "\\weight");
 		for (int i = 0; i < Wnum; i++) {
 			//out.write((char*)&k[i], sizeof k);
 			Tweight >> weight[i];
 			//w << weight[i] << " ";
 		}
-	}
+	}*/
 
 	for (int i = 0; i < Wnum; i++) {
 		delw[i] = 0;
@@ -74,8 +75,8 @@ int main() {
 		outO[i] = 0;
 	}
 
-	double error;
-	int t = 0;
+	float error;
+	time_t t = 0;
 	unsigned int num;
 	std::cout << "Enter the number of epochs: ";
 	std::cin >> num;
@@ -85,19 +86,9 @@ int main() {
 		error = 0;
 		for (int k = 0; k < n[coat - 1]; k++) {
 			image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png");
-			if (!image.data) {
-				std::cout << "Error upload image " << "D:\\Foton\\ngnl_data\\training\\" + std::to_string(k) + "\\1 (" + std::to_string(epoch % 5000 + 1) + ").png";
-				return -1;
-			}
 			Image(image, out, 0);
 			for (int i = 0; i < (coat - 1); i++) {
 				SumFunc(out, weight, n, i);
-			}
-			for (int i = 0; i < n[coat - 1]; i++) {
-				if (out[Onum - n[coat - 1] + i] != out[Onum - n[coat - 1] + i]) {
-					std::cout << "NaN error";
-					return 2;
-				}
 			}
 
 			outO[k] = 1.0;
@@ -110,53 +101,24 @@ int main() {
 			for (int i = 0; i < n[coat - 1]; i++) {
 				error = error + (outO[i] - out[Onum - n[coat - 1] + i]) * (outO[i] - out[Onum - n[coat - 1] + i]) / n[coat - 1];
 			}
-			//error = error / n[coat - 1];
 			outO[k] = 0.0;
 		}
 
+		 //консоль
 		if (time(NULL) > t) {
 			t = time(NULL);
-			std::string sec = "", min = "", hour = "";
-			for (int i = 0; i < n[coat - 1]; i++) {
-				if (out[Onum - n[coat - 1] + i] != out[Onum - n[coat - 1] + i]) {
-					std::cout << "NaN error";
-					return 2;
-				}
-			}
-
-			time_t now = time(NULL);
-			tm* ltm = localtime(&now);
-			std::cout << "                                                  \r";
-			if (ltm->tm_sec < 10) {
-				sec = "0";
-			}
-			if (ltm->tm_min < 10) {
-				min = "0";
-			}
-			if (ltm->tm_hour < 10) {
-				hour = "0";
-			}
-			std::cout << "[" << hour << ltm->tm_hour << ":" << min << ltm->tm_min << ":" << sec << ltm->tm_sec << "][" << epoch << "][" << std::setprecision(5) << error / n[coat - 1] << "] \r";
+			Time(d, epoch, error, n, coat);
 		}
 	}
 
 	std::cout << std::endl << std::endl;
-	//Test(out, weight, coat, n, d);
 	GlobalTime(clock() - d);
 	Testing(image, out, weight, coat, n);
-
+	
 
 	/*--------------------------Backup--------------------------*/
 
-	std::ofstream Tweight(path + "\\weight"/*, std::ios::binary | std::ios::out*/);
-	for (int i = 0; i < Wnum; i++) {
-		Tweight << weight[i] << " ";
-	}
-	if (ProgramData[0] == "0") {
-		std::ofstream Configurations(path + "\\config");
-		Configurations << "LastSave=1" << std::endl
-			<< "Step=" << ProgramData[1];
-	}
+	//BackupWeight(path, ProgramData, Wnum, weight);
 
 	/*--------------------------Backup--------------------------*/
 
@@ -164,6 +126,8 @@ int main() {
 	std::cout << std::endl;
 	std::cout << "Past your path" << std::endl;
 
+
+	//ручное тестирование
 	for (;;) {
 		std::string OrigName, name;
 		getline(std::cin, OrigName);
