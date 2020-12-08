393ed4e82436882609b7e2106c7618a2d153f080
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-12-08 22:58:51 +0300

    Update main.cpp

--- libtorch/main.cpp
+++ libtorch/main.cpp
@@ -20,43 +20,6 @@ const int Saving = 200;
 //const bool Loading = true;
 const bool Loading = false;
 
-void Out(torch::Tensor tensor, int k) {
-    cv::Mat mat = cv::Mat(28, 28, CV_32FC1, tensor.data_ptr());
-
-    if (k % 10 == 0) {
-        cv::Mat image(28, 28, CV_8UC1);
-        for (int i = 0; i < 28; i++) {
-            for (int j = 0; j < 28; j++) {
-                image.at<uchar>(i, j) = ceil(mat.at<float>(i, j) * 255);
-            }
-        }
-        cv::imwrite("D:\\Foton\\ngnl_data\\gen_image\\test\\" + std::to_string(time(NULL)) + ".png", image);
-    }
-    cv::imshow("Out", mat);
-    cv::waitKey(1);
-}
-
-void ConsoleData(int epoch, int Epochs, int batch_index, int batches_per_epoch, float d_loss, float g_loss) {
-	time_t t = time(NULL);
-	std::string sec = "", min = "", hour = "";
-
-	time_t now = time(NULL);
-	tm* ltm = localtime(&now);
-	std::cout << "                                                  \r";
-	if (ltm->tm_sec < 10) {
-		sec = "0";
-	}
-	if (ltm->tm_min < 10) {
-		min = "0";
-	}
-	if (ltm->tm_hour < 10) {
-		hour = "0";
-	}
-	std::cout << "[" << hour << ltm->tm_hour << ":" << min << ltm->tm_min << ":" << sec << ltm->tm_sec << "][" << epoch /*<< std::setprecision(5) */
-		<< "/" << Epochs << "][" << batch_index << "/" << batches_per_epoch << "] Dis_loss: "<< std::setprecision(4) << d_loss << " | Gen_loss: " << std::setprecision(4) << g_loss << "\r";
-
-}
-
 using namespace torch;
 
 struct DCGANGeneratorImpl:
@@ -153,6 +116,23 @@ int main() {
         for (torch::data::Example<>& batch : *data_loader) {
             discriminator->zero_grad();
             torch::Tensor real_images = batch.data.to(device);
+
+            torch::Tensor tensor_image;
+
+            for (int i = 0; i < batch.data.size(0); i++) {
+                cv::Mat img = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(rand() % 10) + "\\1 (" + std::to_string(rand() % 5000) + ").png", CV_32FC1);
+                //cv::imshow("Out", img);
+                //cv::waitKey(100);
+                tensor_image[i] = torch::from_blob(img.data, { img.rows, img.cols, 1 }, at::kByte);
+                //tensor_image = tensor_image.permute({ 2,0,1 });
+            }
+
+            for (int i = 0; i < batch.data.size(0); i++) {
+                cv::Mat mat = cv::Mat(28, 28, CV_32FC1, tensor_image[i].data_ptr());
+                std::cout << i;
+                cv::imshow("Out", mat);
+                cv::waitKey(100);
+            }
 
             //cv::Mat mat = cv::Mat(28, 28, CV_32FC1, real_images.data_ptr());
             //cv::imshow("Out", mat);
