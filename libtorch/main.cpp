fd53444cd14f1bfde3644a4ea54d6657ec9328cf
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-12-24 18:34:55 +0300

    really big update

--- libtorch/main.cpp
+++ libtorch/main.cpp
@@ -1,30 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <torch/torch.h>
+#include <ATen/ATen.h>
 
 #include <cmath>
 #include <cstdio>
 #include <iostream>
 #include "test.h"
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <string>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 
 
+
+
+
 const int SizeInputG = 100;
 const int Batch = 64;
 const int Epochs = 30;
-const char* path = "D:\\Foton\\xz";
+const char* path = "D:\\Foton\\data\\d";
 const int Saving = 200;
+const int LData = 5000;
 
 //const bool Loading = true;
 const bool Loading = false;
 
 using namespace torch;
 
-struct DCGANGeneratorImpl:
+
+
+struct DCGANGeneratorImpl :
     nn::Module {
-    DCGANGeneratorImpl(int SizeInputG):
+    DCGANGeneratorImpl(int SizeInputG) :
         conv1(nn::ConvTranspose2dOptions(SizeInputG, 256, 4).bias(false)), batch_norm1(256),
         conv2(nn::ConvTranspose2dOptions(256, 200, 3).stride(2).padding(1).bias(false)), batch_norm2(200),
         conv3(nn::ConvTranspose2dOptions(200, 64, 4).stride(2).padding(1).bias(false)), batch_norm3(64),
@@ -51,9 +62,98 @@ struct DCGANGeneratorImpl:
     nn::BatchNorm2d batch_norm1, batch_norm2, batch_norm3;
 };
 
+struct Options {
+    int image_size = 28;
+    size_t train_batch_size = 64;
+    size_t test_batch_size = 200;
+    size_t iterations = 10;
+    size_t log_interval = 20;
+    // path must end in delimiter
+    std::string datasetPath = "D:\\Foton\\ngnl_data\\training\\help\\0\\";
+    std::string infoFilePath = "D:\\Foton\\data\\info.txt";
+    torch::DeviceType device = torch::kCPU;
+};
+
+
+
+using Data = std::vector<std::pair<std::string, long>>;
+static Options options;
+
+
+std::pair<Data, Data> readInfo() {
+    Data train, test;
+
+    std::ifstream stream(options.infoFilePath);
+    assert(stream.is_open());
+
+    long label = 1;
+    std::string path, type;
+    for (int i = 0; i < 5000; i++) {
+        path = std::to_string(i) + ".png";
+        train.push_back(std::make_pair(path, label));
+    }
+    std::random_shuffle(train.begin(), train.end());
+    std::random_shuffle(test.begin(), test.end());
+    return std::make_pair(train, test);
+}
+
+
+class CustomDataset : public torch::data::datasets::Dataset<CustomDataset> {
+    using Example = torch::data::Example<>;
+
+    Data data;
+
+public:
+    CustomDataset(const Data& data) : data(data) {}
+
+    Example get(size_t index) {
+        std::string path = options.datasetPath + data[index].first;
+        auto mat = cv::imread(path);
+        //cv::Mat im = cv::imread("D:\\Foton\\ngnl_data\\img_align_celeba\\000001.jpg");
+        cv::resize(mat, mat, { 64, 64 });
+        //cv::imshow("Out", mat);
+        //cv::waitKey(10);
+        //std::cout << index;
+        /*cv::imshow("Out", mat);
+        cv::waitKey(1);*/
+        assert(!mat.empty());
+
+        cv::resize(mat, mat, cv::Size(options.image_size, options.image_size));
+        std::vector<cv::Mat> channels(3);
+        cv::split(mat, channels);
+
+        /*auto R = torch::from_blob(
+            channels[2].ptr(),
+            { options.image_size, options.image_size },
+            torch::kUInt8);
+        auto G = torch::from_blob(
+            channels[1].ptr(),
+            { options.image_size, options.image_size },
+            torch::kUInt8);*/
+        auto B = torch::from_blob(
+            channels[0].ptr(),
+            { options.image_size, options.image_size },
+            torch::kUInt8);
+
+        auto tdata = torch::cat({ B })
+            .view({ 1, options.image_size, options.image_size })
+            .to(torch::kFloat);
+        auto tlabel = torch::from_blob(&data[index].second, { 1 }, torch::kLong);
+        return { tdata, tlabel };
+    }
+
+    torch::optional<size_t> size() const {
+        return data.size();
+    }
+};
+
 TORCH_MODULE(DCGANGenerator);
 
+
+
+
 int main() {
+
     time_t t = 0;
     torch::manual_seed(1);
     torch::Device device(torch::kCPU);
@@ -62,27 +162,29 @@ int main() {
     generator->to(device);
 
     nn::Sequential discriminator(
-        nn::Conv2d(nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).bias(false)), 
+        nn::Conv2d(nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).bias(false)),
         nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
 
-        nn::Conv2d(nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).bias(false)), 
+        nn::Conv2d(nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).bias(false)),
         nn::BatchNorm2d(128), nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
 
         nn::Conv2d(nn::Conv2dOptions(128, 256, 4).stride(2).padding(1).bias(false)),
         nn::BatchNorm2d(256),
         nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
 
-        nn::Conv2d(nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).bias(false)), 
+        nn::Conv2d(nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).bias(false)),
         nn::Sigmoid());
     discriminator->to(device);
 
-    auto dataset = torch::data::datasets::MNIST(path).map(torch::data::transforms::Normalize<>(0.5, 0.5)).map(torch::data::transforms::Stack<>());
-    const int batches_per_epoch = std::ceil(dataset.size().value() / static_cast<double>(Batch));
-
-    auto data_loader = torch::data::make_data_loader(std::move(dataset), torch::data::DataLoaderOptions().batch_size(Batch).workers(2));
+    auto data = readInfo();
+    auto train_set = CustomDataset(data.first).map(torch::data::transforms::Stack<>());
+    auto data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(std::move(train_set), Batch);
 
     torch::optim::Adam generator_optimizer(generator->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple(0.5, 0.5)));
     torch::optim::Adam discriminator_optimizer(discriminator->parameters(), torch::optim::AdamOptions(2e-4).betas(std::make_tuple(0.5, 0.5)));
+
+
+
 
     if (Loading) {
         torch::load(generator, "D:\\Foton\\xz\\dat\\generator-checkpoint.pt");
@@ -94,70 +196,16 @@ int main() {
     int checkpoint_counter = 1;
     for (int epoch = 0; epoch < Epochs; epoch++) {
         int batch_index = 0;
-        for (torch::data::Example<>& batch : *data_loader) {
+        for (auto& batch : *data_loader) {
             discriminator->zero_grad();
             torch::Tensor real_images = batch.data.to(device);
-
-            torch::Tensor tensor_image[Batch];
-            //torch::Tensor real_images;
-            srand(static_cast<unsigned long long>(time(NULL)));
-            for (int k = 0; k < batch.data.size(0); k++) {
-                cv::Mat image = cv::imread("D:\\Foton\\ngnl_data\\training\\" + std::to_string(rand() % 10) + "\\1 (" + std::to_string(rand() % 5000 + 1) + ").png", CV_32FC1);
-                cv::Mat im(28, 28, CV_32FC1);
-                //cv::threshold(image, image, -1, 1, CV_32FC1);
-                for (int i = 0; i < 28; i++) {
-                    for (int j = 0; j < 28; j++) {
-                        //std::cout << (int)image.at<uchar>(i, j) << std::endl;
-                        im.at<float>(i, j) = -1 + (float)2 * image.at<uchar>(i, j) / 255;
-
-                        //std::cout << (float)im.at<float>(i, j) << std::endl;
-                    }
-                }
-
-                //cv::imshow("Ou5t", im);
-
-
-                std::vector<int64_t> sizes = { 1, 3, image.rows, image.cols };
-                at::TensorOptions options(at::ScalarType::Byte);
-                at::Tensor tensor_image = torch::from_blob(image.data, at::IntList(sizes), options);
-                //tensor_image = tensor_image.toType(at::kFloat);
-
-                //ten = torch::cat((ten, tensor), 0);
-
-                cv::Mat mat = cv::Mat(image.rows, image.cols, image.type(), tensor_image.data_ptr());
-                //tensor_image[k] = tensor;
-                //real_images = torch::from_blob(tensor.data_ptr(), {28, 28, 1, k } );
-               /* cv::Mat mat = cv::Mat(28, 28, CV_32FC1, tensor.data_ptr());
-                cv::imshow("Out", mat);
-                cv::waitKey(1);*/
-
-                //cv::Mat mat = cv::Mat(28, 28, CV_32FC1, input_tensor.data_ptr());
-
-                //cv::imshow("Out", img);
-                //cv::waitKey(100);
-                //tensor_image[i] = torch::from_blob(img.data, { img.rows, img.cols, 1}, at::kFloat);
-                //tensor_image = tensor_image.permute({ 2,0,1 });
-            }
-
-            //tensor_image = torch::from_blob(inputs.data(), {28, 28, 1, Batch});
-           /* for (int i = 0; i < batch.data.size(0); i++) {
-                
-                cv::Mat mat = cv::Mat(28, 28, CV_32FC1, real_images[i].data_ptr());
-                std::cout << i;
-                cv::imshow("Out", mat);
-                cv::waitKey(100);
-            }*/
-
-            //cv::Mat mat = cv::Mat(28, 28, CV_32FC1, real_images.data_ptr());
-            //cv::imshow("Out", mat);
-            //cv::waitKey(1);
-
             torch::Tensor real_labels = torch::empty(batch.data.size(0), device).uniform_(0.8, 1.0);            //uniform нормализует дикий рандом, судя по всему это тензор прав результатов для настоящих изобр
+
+
             //std::cout << real_labels << std::endl;
             torch::Tensor real_output = discriminator->forward(real_images);
             torch::Tensor d_loss_real = torch::binary_cross_entropy(real_output, real_labels);
             d_loss_real.backward();
-
             torch::Tensor noise = torch::randn({ batch.data.size(0), SizeInputG, 1, 1 }, device);
             torch::Tensor fake_images = generator->SumReTa(noise);
             torch::Tensor fake_labels = torch::zeros(batch.data.size(0), device);             //нулевые значения для генератора
