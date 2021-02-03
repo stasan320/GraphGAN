1125fcabc13c5c1f1a8e384b849fd38af7e914e6
stasan320 <44366841+stasan320@users.noreply.github.com>
2021-02-03 14:52:40 +0300

    Create test.cpp

---
+++ CPU/perceptron/test.cpp
@@ -0,0 +1,20 @@
+	//Conv Neural
+  
+  for (int k = 0; k < 7; k++) {
+		for (int l = 0; l < 7; l++) {
+			for (int m = 0; m < 4; m++) {
+				int SvNum = k * 4 * 28 + 4 * l + m * 28;
+				for (int i = SvNum; i < SvNum + 4; i++) {
+					std::cout << i << " ";
+				}
+				std::cout << std::endl;
+			}
+			/*for (int i = k * 2 * 28 + 4 * l + 28; i < k * 2 * 28 + 4 * l + 4 + 28; i++) {
+				std::cout << i << " ";
+			}*/
+			std::cout << std::endl;
+			std::cout << std::endl;
+		}
+
+
+	} 
