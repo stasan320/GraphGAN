9cd2ad1ac721a3659e771e72acece6827e37fcb3
stasan320 <44366841+stasan320@users.noreply.github.com>
2021-02-06 20:32:44 +0300

    Update main.cpp

--- CPU/perceptron/min_max/main.cpp
+++ CPU/perceptron/min_max/main.cpp
@@ -5,20 +5,10 @@
 #include "constant.h"
 
 
-const int LayersNum = 3;
+const int LayersNum = 4;
 
 int main() {
-	int n[LayersNum] = { 784, 8, 1 }, Onum = 0, Dnum = 0, Wnum = 0;
-	//std::cout << Path;
-	//return 0;
-	//std::string ConfigurationPath(const std::wstring & Path);
-	/*std::ifstream Configuration(path + "\\config");
-	ProgramConst(ProgramData);
-	step = std::stod(ProgramData[1]);*/
-
-	/*while(Configuration) {
-		Configuration >> backup_weight;
-	}*/
+	int n[LayersNum] = { 784, 50, 20, 1 }, Onum = 0, Dnum = 0, Wnum = 0;
 
 	for (int i = 0; i < LayersNum; i++) {
 		Onum = Onum + n[i];
@@ -33,7 +23,7 @@ int main() {
 	float* outO = new float[n[LayersNum - 1]];
 	//float* del = new float[Onum - n[LayersNum - 1]] (0);
 	float* weight = new float[Wnum];
-	float* delw = new float[Wnum];
+	std::vector<float> delw(Wnum, 0);
 
 	Random(weight, -1, 1, 0, Wnum, clock());
 
@@ -45,8 +35,8 @@ int main() {
 	Input[2] = 1;   //weight
 	Input[3] = 0;   //d
 
-	for (int epoch = 0; epoch < 10000; epoch++) {
-		cv::Mat image = cv::imread("F:\\Foton\\ngnl_data\\training\\help\\" + std::to_string(rand() % 10) + "\\" + std::to_string(epoch % 5000) + ".png");
+	for (int epoch = 0; epoch < 1000; epoch++) {
+		cv::Mat image = cv::imread("F:\\Foton\\ngnl_data\\training\\help\\" + std::to_string(0) + "\\" + std::to_string(epoch % 5000) + ".png");
 		Image(image, out, 0);
 		for (int i = 0; i < (LayersNum - 1); i++) {
 			SumFunc(out, weight, n, i);
@@ -55,7 +45,7 @@ int main() {
 		outOld = out;
 		float out1 = out[Onum - n[LayersNum - 1]];
 
-		image = cv::imread("F:\\Foton\\ngnl_data\\gen_image\\" + std::to_string(epoch % 1000) + ".png");
+		image = cv::imread("F:\\Foton\\ngnl_data\\gen_image\\" + std::to_string(epoch %5000) + ".png");
 		Image(image, out, 0);
 		for (int i = 0; i < (LayersNum - 1); i++) {
 			SumFunc(out, weight, n, i);
@@ -66,7 +56,7 @@ int main() {
 		float LossFucntion = log(out1) + log(1 - out2);
 		//float LossFunctionD = ()
 
-		std::cout << out1 << "   " << out2 << "   " << LossFucntion << std::endl;
+		std::cout << out1 << "   " << out2 << "   " << "LossD: " << LossFucntion << std::endl;
 
 		std::vector<float>del(Onum, 0);
 		for (int i = 0; i < n[LayersNum - 1]; i++) {
@@ -78,17 +68,19 @@ int main() {
 				float delta = (1 - out1) * outOld[OutNum + j] - out2 * out[OutNum + j];
 				weight[WeightNum] = weight[WeightNum] + Step * delta;
 
-				del[i] = del[i] + delta;
+				del[j] = delta;
 			}
+		}
+
+		for (int i = 0; i < (LayersNum - 2); i++) {
+			Iter(out, weight, delw, del, n, i, LayersNum);
 		}
 
 		for (int i = 0; i < n[LayersNum - 1]; i++) {
 			//std::cout << del[i] << std::endl;
 		}
 
-		for (int i = 0; i < (LayersNum - 2); i++) {
-			//Iter(out, weight, delw, del, n, i, LayersNum);
-		}
-		return 0;
+
+		//return 0;
 	}
 }
