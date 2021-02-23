f59ed96cc7886787ce0531720c3e591b376f0a52
stasan320 <44366841+stasan320@users.noreply.github.com>
2021-02-23 15:24:50 +0300

    Update constant.h

--- CPU/GAN/min_max/constant.h
+++ CPU/GAN/min_max/constant.h
@@ -9,20 +9,20 @@
 
 typedef unsigned long long ull;
 
-int D_max = 64;
-int G_max = 64;
-const int image_size = 256;
-const int LayersNumD = 3;
+int DMAX = 128;
+int GMAX = 128;
+const int image_size = 64;
+const int LayersNumD = 2;
 const int LayersNumG = 3;
-const float StepD = 0.25;
-const float StepG = 0.25;
+const float StepD = 0.3;
+const float StepG = 0.3;
 const float Leaky = 0.05;
 
 struct Discriminator {
 	ull Onum = 0;
 	ull Wnum = 0;
 	ull Dnum = 0;
-	ull Layer[LayersNumD] = { image_size * image_size, 10, 1 };
+	ull Layer[LayersNumD] = { image_size * image_size, 1 };
 };
 struct Generator {
 	ull Onum = 0;
@@ -55,61 +55,6 @@ void Exp(std::vector<float>& out, std::vector<float>& weight, ull* n, ull num) {
 
 		Onum += n[L];
 		Wnum += n[L] * n[L + 1];
-	}
-}
-
-void ConvExp(std::vector<float>& out, std::vector<float>& weight, ull* n, ull num) {
-	ull Onum = 0, Wnum = 0;
-
-	for (ull i = 0; i < num; i++) {
-		Onum = Onum + n[i];
-		Wnum = Wnum + n[i] * n[i + 1];
-	}
-
-	for (ull i = 0; i < n[num + 1]; i++) {
-		float sum = 0;
-		for (ull j = 0; j < n[num]; j++) {
-			sum += weight[Wnum + j + i * n[num]] * out[Onum + j];
-		}
-		out[Onum + n[num] + i] = 1.0 / (1.0 + exp(-sum));
-	}
-}
-
-void Tanh(std::vector<float>& out, std::vector<float>& weight, ull* n, ull num) {
-	ull Onum = 0, Wnum = 0;
-	for (ull i = 0; i < num; i++) {
-		Onum = Onum + n[i];
-		Wnum = Wnum + n[i] * n[i + 1];
-	}
-
-	for (ull i = 0; i < n[num + 1]; i++) {
-		float sum = 0;
-		for (ull j = 0; j < n[num]; j++) {
-			sum += weight[Wnum + j + i * n[num]] * out[Onum + j];
-		}
-		out[Onum + n[num] + i] = 2.0 / (1.0 + exp(-2 * sum)) - 1;
-	}
-}
-
-void ReLu(std::vector<float>& out, std::vector<float>& weight, ull* n, ull num) {
-	ull Onum = 0, Wnum = 0;
-	for (ull i = 0; i < num; i++) {
-		Onum = Onum + n[i];
-		Wnum = Wnum + n[i] * n[i + 1];
-	}
-
-	for (ull i = 0; i < n[num + 1]; i++) {
-		float sum = 0;
-		for (ull j = 0; j < n[num]; j++) {
-			sum += weight[Wnum + j + i * n[num]] * out[Onum + j];
-		}
-		//out[Onum + n[num] + i] = 1.0 / (1.0 + exp(-net));
-		if (sum < 0) {
-			out[Onum + n[num] + i] = Leaky * sum;
-		}
-		else {
-			out[Onum + n[num] + i] = sum;
-		}
 	}
 }
 
@@ -160,53 +105,37 @@ void Iter(std::vector<float>& out, std::vector<float>& outOld, std::vector<float
 				weight[WeightNum] = weight[WeightNum] + StepD * (delOld[Dnum + j] - del[Dnum + j]);
 			}
 		}
-
-		//Dnum += n[coat - L - 1];
 	}
 }
 
 void BatchTanh(cv::Mat image, std::vector<float>& out, ull Onum) {
-	for (ull i = 0; i < image.rows; i++) {
-		for (ull j = 0; j < image.cols; j++) {
+	for (ull i = 0; i < image.rows; i++)
+		for (ull j = 0; j < image.cols; j++)
 			out[Onum + i * image.cols + j] = -1 + image.at<uchar>(i, j) / 255.0 * 2.0;
-			//std::cout << out[Onum + i * image.cols + j] << "   " << image.at<uchar>(i, j)<< std::endl;
-		}
-	}
-
 }
 
 void OutTanh(std::vector<float>& out, ull Onum) {
 	cv::Mat image(image_size, image_size, CV_8UC1);
-
-	for (ull i = 0; i < image.rows; i++) {
-		for (ull j = 0; j < image.cols; j++) {
+	for (ull i = 0; i < image.rows; i++)
+		for (ull j = 0; j < image.cols; j++)
 			image.at<uchar>(i, j) = ceil((1 + out[Onum + j + i * image.cols]) / 2.0 * 255);
-			//std::cout << (1 + out[Onum + j + i * image.cols]) / 2.0 * 255 << std::endl;
-		}
-	}
-	//exit(0);
+
 	cv::imshow("Out", image);
 	cv::waitKey(1);
 }
 
 void BatchExp(cv::Mat image, std::vector<float>& out, ull Onum) {
-	for (ull i = 0; i < image.rows; i++) {
-		for (ull j = 0; j < image.cols; j++) {
+	for (ull i = 0; i < image.rows; i++)
+		for (ull j = 0; j < image.cols; j++)
 			out[Onum + i * image.cols + j] = image.at<uchar>(i, j) / 255.0;
-			//std::cout << out[Onum + i * image.cols + j] << "   " << image.at<uchar>(i, j)<< std::endl;
-		}
-	}
-
 }
 
 void Out(std::vector<float>& out, ull Onum) {
 	cv::Mat image(image_size, image_size, CV_8UC1);
-
-	for (ull i = 0; i < image.rows; i++) {
-		for (ull j = 0; j < image.cols; j++) {
+	for (ull i = 0; i < image.rows; i++)
+		for (ull j = 0; j < image.cols; j++)
 			image.at<uchar>(i, j) = out[Onum + j + i * image.cols] * 255;
-		}
-	}
+
 	cv::imshow("Out", image);
 	cv::waitKey(1);
 }
