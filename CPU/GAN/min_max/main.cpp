c662ce418b754895535b187c4af2ad1dcdca3c5a
stasan320 <44366841+stasan320@users.noreply.github.com>
2021-02-23 15:24:27 +0300

    Update main.cpp

--- CPU/GAN/min_max/main.cpp
+++ CPU/GAN/min_max/main.cpp
@@ -1,4 +1,4 @@
-#include "Header.h"
+#include "constant.h"
 
 int main() {
 
@@ -26,7 +26,6 @@ int main() {
 	std::vector<float> GoutOld(generator.Onum, 0);
 	std::vector<float> Gweight(generator.Wnum);
 	std::vector<float> Gdel(discriminator.Onum + generator.Onum, 0);
-	std::vector<float> GdelOld(discriminator.Onum, 0);
 
 	Random(weight, -1, 1, 0, discriminator.Wnum, clock());
 	Random(Gweight, -1, 1, 0, generator.Wnum, clock());
@@ -37,14 +36,9 @@ int main() {
 
 
 	for (ull epoch = 0; epoch < 10000; epoch++) {
-		for (ull D = 0; D < D_max; D++) {
-			cv::Mat image = cv::imread("G:\\Foton\\data\\cropped\\1 (" + std::to_string((epoch * D_max + D + dop) % 5000 + 1) + ").jpg", CV_8UC1);
-			if (!image.data) {
-				dop++;
-				image = cv::imread("G:\\Foton\\data\\cropped\\1 (" + std::to_string((epoch * D_max + D + dop) % 5000 + 1) + ").jpg", CV_8UC1);
-			}
-			//cv::Mat image = cv::imread("C:\\Users\\stasan\\Downloads\\archive\\mnist_png\\testing\\" + std::to_string(rand() % 10) + "\\1 (" + std::to_string((epoch * D_max + D) % 890 + 1) + ").png", CV_8UC1);
-
+		for (ull D = 0; D < DMAX; D++) {
+			cv::Mat image = cv::imread("F:\\Foton\\ngnl_data\\training\\help\\anime\\" + std::to_string((D + epoch * DMAX) % 60000) + ".png", CV_8UC1);
+			//cv::Mat image = cv::imread("F:\\Foton\\ngnl_data\\training\\help\\" + std::to_string(rand() % 10) + "\\" + std::to_string((epoch * DMAX + D) % 6000 + 1) + ".png", CV_8UC1);
 
 			cv::resize(image, image, cv::Size(image_size, image_size));
 			BatchExp(image, out, 0);
@@ -53,7 +47,7 @@ int main() {
 			outReal = out;
 			float out1 = out[discriminator.Onum - discriminator.Layer[LayersNumD - 1]];
 
-			Random(Gout, -0.5, 0, 0, generator.Layer[0], clock());
+			Random(Gout, 0, 1, 0, generator.Layer[0], clock());
 			Exp(Gout, Gweight, generator.Layer, LayersNumG);
 			for (ull i = 0; i < generator.Layer[LayersNumG - 1]; i++) {
 				out[i] = Gout[generator.Onum - generator.Layer[LayersNumG - 1] + i];
@@ -87,8 +81,8 @@ int main() {
 		}
 
 
-		for (ull G = 0; G < G_max; G++) {
-			Random(Gout, -0.5, 0, 0, generator.Layer[0], clock());
+		for (ull G = 0; G < GMAX; G++) {
+			Random(Gout, 0, 1, 0, generator.Layer[0], clock());
 			Exp(Gout, Gweight, generator.Layer, LayersNumG);
 
 			for (ull i = 0; i < generator.Layer[LayersNumG - 1]; i++) {
@@ -103,74 +97,57 @@ int main() {
 			std::cout << out2 << "   " << "LossG: " << LossFucntionG << std::endl;
 
 			ull WeightDopNum = 0, OutDopNum = 0, DelDopNum = 0;
-			WeightDopNum += discriminator.Layer[LayersNumD - 2];
+			WeightDopNum += discriminator.Layer[LayersNumD - 2] * discriminator.Layer[LayersNumD - 1];
 			OutDopNum += discriminator.Layer[LayersNumD - 1] + discriminator.Layer[LayersNumD - 2];
 
-			for (ull i = 0; i < discriminator.Layer[LayersNumD - 2]; i++) {
-				ull WeightNum = discriminator.Wnum - WeightDopNum + i;
-				ull OutNum = discriminator.Onum - OutDopNum + i;
+			/*for (ull i = 0; i < discriminator.Layer[1]; i++) {
+				ull WeightNum = discriminator.Layer[0] * discriminator.Layer[1] + i;
+				ull OutNum = discriminator.Layer[0] + i;
+
+				Gdel[i] = (1 - out2) * weight[WeightNum] * out[OutNum] * (1 - out[OutNum]);
+			}*/
+
+			for (ull i = 0; i < discriminator.Layer[0]; i++) {
+				ull WeightNum = i;
+				ull OutNum = i;
 
 				Gdel[i] = (1 - out2) * weight[WeightNum] * out[OutNum] * (1 - out[OutNum]);
 			}
 
-			/*if (LayersNumD == 3) {
-				for (ull i = 0; i < discriminator.Layer[0]; i++) {
-					float Sum = 0;
-					for (ull j = 0; j < discriminator.Layer[1]; j++) {
-						ull WeightNum = i + j * discriminator.Layer[0];
-						Sum += Gdel[j] * weight[WeightNum];
-					}
-					Gdel[discriminator.Layer[1] + i] = Sum * out[i] * (1 - out[i]);
+			/*for (ull i = 0; i < discriminator.Layer[0]; i++) {
+				float Sum = 0;
+				for (ull j = 0; j < discriminator.Layer[1]; j++) {
+					ull WeightNum = i + j * discriminator.Layer[0];
+					Sum += Gdel[j] * weight[WeightNum];
 				}
-
-				DelDopNum += discriminator.Layer[1];
-			}*/
-
-			for (ull num = 0; num < (LayersNumD - 2); num++) {
-				WeightDopNum += discriminator.Layer[LayersNumD - 2 - num] * discriminator.Layer[LayersNumD - 3 - num];
-				OutDopNum += discriminator.Layer[LayersNumD - 2 - num];
-
-				for (ull i = 0; i < discriminator.Layer[LayersNumD - 3 - num]; i++) {
-					float sum = 0;
-					for (ull j = 0; j < discriminator.Layer[LayersNumD - 2 - num]; j++) {
-						ull WeightNum = discriminator.Wnum - WeightDopNum + i + j * discriminator.Layer[LayersNumD - 3 - num];
-						//int OutNum = discriminator.Onum - OutDopNum + j;
-
-						sum += (Gdel[DelDopNum + j] * weight[WeightNum])/* / (float)discriminator.Layer[LayersNumD - 2 - num]*/;
-					}
-					ull OutNum = discriminator.Onum - OutDopNum - discriminator.Layer[LayersNumD - 3 - num] + i;
-					Gdel[DelDopNum + discriminator.Layer[LayersNumD - 2 - num] + i] = sum * out[OutNum] * (1 - out[OutNum]);
-				}
-				DelDopNum += discriminator.Layer[LayersNumD - 2 - num];
+				Gdel[discriminator.Layer[1] + i] = Sum * out[i] * (1 - out[i]);
 			}
+			DelDopNum += discriminator.Layer[1];*/
 			WeightDopNum = 0, OutDopNum = 0;
 
-			for (ull num = 0; num < (LayersNumG - 2); num++) {
-				WeightDopNum += generator.Layer[LayersNumG - 2 - num] * generator.Layer[LayersNumG - 1 - num];
-				OutDopNum += generator.Layer[LayersNumG - 2 - num];
 
-				for (ull i = 0; i < generator.Layer[LayersNumG - 2 - num]; i++) {
-					float per = 0;
-					ull OutNum = generator.Onum - OutDopNum + i;
+			for (ull i = 0; i < generator.Layer[1]; i++) {
+				float per = 0;
+				ull OutNum = generator.Layer[0] + i;
 
-					for (ull j = 0; j < generator.Layer[LayersNumG - 1 - num]; j++) {
-						ull WeightNum = generator.Wnum - WeightDopNum + i + j * generator.Layer[LayersNumG - 2 - num];
-						Gweight[WeightNum] = Gweight[WeightNum] + Step * Gdel[DelDopNum + j] * Gout[OutNum];
+				for (ull j = 0; j < generator.Layer[2]; j++) {
+					ull WeightNum = generator.Layer[0] * generator.Layer[1] + i + j * generator.Layer[1];
+					per += Gdel[DelDopNum + j] * Gweight[WeightNum];
 
-						per += Gdel[DelDopNum + j] * Gweight[WeightNum];
-					}
+					Gweight[WeightNum] = Gweight[WeightNum] + StepG * Gdel[DelDopNum + j] * Gout[OutNum];
 
-					Gdel[DelDopNum + generator.Layer[LayersNumG - 1 - num] + i] = per * (1 - Gout[OutNum]) * Gout[OutNum];
 				}
-				DelDopNum += generator.Layer[LayersNumG - 1 - num];
+
+				Gdel[DelDopNum + generator.Layer[2] + i] = per * (1 - Gout[OutNum]) * Gout[OutNum];
 			}
+			DelDopNum += generator.Layer[2];
 
 
 			for (ull i = 0; i < generator.Layer[0]; i++) {
 				for (ull j = 0; j < generator.Layer[1]; j++) {
 					ull WeightNum = i + j * generator.Layer[0];
 
-					Gweight[WeightNum] = Gweight[WeightNum] + Step * Gdel[DelDopNum + j] * Gout[i];
+					Gweight[WeightNum] = Gweight[WeightNum] + StepG * Gdel[DelDopNum + j] * Gout[i];
 				}
 			}
 		}
